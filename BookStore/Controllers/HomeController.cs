6deab317eb8ba68d88b2f947aa4595667e46f3bc
RoshanLiu <xygenio@hotmail.com>
2023-02-19 02:23:06 +1100

    delete event interface

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using BookStore.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using System.Security.Claims;
@@ -16,31 +17,31 @@ namespace BookStore.Controllers
 
         private readonly ILogger<HomeController> _logger;
         private readonly IConfiguration _configuration;//for accessing keys
-        private readonly UserContext _userContext;//for record the stock states of each users
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserContext _userContext;//Projections
+        private readonly EventContext _eventContext;
 
-
+        
 
         public HomeController(
             ILogger<HomeController> logger,
             IConfiguration configuration,
             UserContext userContext,
-            UserManager<IdentityUser> userManager
+            EventContext eventContext
+            
             )
         {
             _logger = logger;
             _configuration = configuration;
             _userContext = userContext;
-            _userManager = userManager;
+            _eventContext = eventContext;
         }
-
         [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> Index()
         {
 
             ViewBag.BookList = new List<BookModel>();
-            var store = await _userContext.Users.FirstOrDefaultAsync(u => u.id == "bookstore");
+            var store = await _userContext.Users.SingleOrDefaultAsync(u => u.id == "bookstore");
             if (store != null)
             {
                 ViewBag.BookList = store.Books;
@@ -53,40 +54,51 @@ namespace BookStore.Controllers
         [HttpGet]
         public async Task<IActionResult> Reserved()
         {
-            var loggedUser = User.FindFirstValue(ClaimTypes.Name);
-            ViewBag.LoggedUser = loggedUser;
-            var user = await _userContext.Users.FirstOrDefaultAsync(u => u.id == loggedUser);
-            if(user != null)
-            {
-                return View(user);
-            }
-            else//if user stock record doesn't exist, create one with empty book list
-            {
-                UserModel newUser = new UserModel() {
-                    id = loggedUser,
-                    Books= new List<BookModel>()
-                };
-                await _userContext.Users.AddAsync(newUser);
-                await _userContext.SaveChangesAsync();
-                return RedirectToAction("Reserved");
-            }
+            var user = await GetCurrentUser();
+            return View(user);
         }
 
-        [HttpPost]
-        public async Task<IActionResult> ReserveBook()
+        public async Task<IActionResult> ReserveBook(string id)
         {
-
-
-
-
+            
             return RedirectToAction("index");
         }
 
 
 
+
+
+        ///Get current logged user's stock.
+        public async Task<UserModel> GetCurrentUser() {
+            var loggedUser = User.FindFirstValue(ClaimTypes.Name);
+            var userStock = await _userContext.Users.SingleOrDefaultAsync(u => u.id == loggedUser);
+            if (userStock != null)
+            {
+                return userStock;
+            }
+            else
+            {
+                var newUser = new UserModel()
+                {
+                    id = loggedUser,
+                    Books = new List<BookModel>()
+                };
+                await _userContext.Users.AddAsync(newUser);
+                await _userContext.SaveChangesAsync();
+                userStock = await _userContext.Users.SingleOrDefaultAsync(u => u.id == loggedUser);
+                return newUser;
+            }
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+
+
+
+
     }
 }
--- BookStore/Models/EventModel.cs
+++ BookStore/Models/EventModel.cs
@@ -4,7 +4,7 @@
     {
         public Guid Id { get; set; }
         public string? BookId { get; set; }
-        public int Quantity { get; set; }
+        public bool Quantity { get; set; }
         public DateTime DateTime { get; set; }
         public string? UserId { get; set; }
     }
